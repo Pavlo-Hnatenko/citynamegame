f8d9026790681d414c344c0970ab3b7aad292865
Pavlo-Hnatenko <gnatenko.pavlo@gmail.com>
2020-09-30 08:37:33 +0300

    fourth commit: the length of long lines was changed

--- src/main/java/com/citiesgame/logic/GameLogic.java
+++ src/main/java/com/citiesgame/logic/GameLogic.java
@@ -13,23 +13,18 @@ public class GameLogic {
 
     private static final String START_CITY = "Kharkiv";
     private static final String[] CITIES_VALUES = new String[]{
-            "Ankara", "Budapest", "Canberra", "Dhaka", "Edinburgh", "Freetown",
-            "Gdansk", "Honiara", "Islamabad", "Juba", "Kyiv", "London",
-            "Montevideo", "Niamey", "Oslo", "Podgorica", "Quito", "Roseau",
-            "Santiago", "Thimphu", "Ulaanbaatar", "Vatican", "Warsaw", "Xining",
-            "Yerevan", "Zanzibar"
+            "Ankara", "Budapest", "Canberra", "Dhaka", "Edinburgh", "Freetown", "Gdansk", "Honiara", "Islamabad", "Juba",
+            "Kyiv", "London", "Montevideo", "Niamey", "Oslo", "Podgorica", "Quito", "Roseau", "Santiago", "Thimphu",
+            "Ulaanbaatar", "Vatican", "Warsaw", "Xining", "Yerevan", "Zanzibar"
     };
-    private static final Set<String> CITIES =
-            new HashSet<>(Arrays.asList(CITIES_VALUES));
+    private static final Set<String> CITIES = new HashSet<>(Arrays.asList(CITIES_VALUES));
 
     public static void startGame() {
 
-        System.out.println("Let's start from the city: "
-                + START_CITY + System.lineSeparator() + "Your turn!");
+        System.out.println("Let's start from the city: " + START_CITY + System.lineSeparator() + "Your turn!");
 
         while (true) {
-            BufferedReader reader =
-                    new BufferedReader(new InputStreamReader(System.in));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             String inputCity;
 
             try {
@@ -40,9 +35,8 @@ public class GameLogic {
                     systemAnswerCity(inputCity);
                     return;
                 } else {
-                    throw new IncorrectCityException("The city " + inputCity
-                            + " that you entered is incorrect answer!" +
-                            " Please, enter the correct city.");
+                    throw new IncorrectCityException("The city " + inputCity + " that you entered is incorrect answer!"
+                            + " Please, enter the correct city.");
                 }
 
             } catch (IncorrectCityException | IOException e) {
@@ -51,24 +45,19 @@ public class GameLogic {
         }
     }
 
-    private static boolean isSuitableCity(String previousCity,
-                                          String currentCity) {
+    private static boolean isSuitableCity(String previousCity, String currentCity) {
 
-        return currentCity.regionMatches(true, 0, previousCity,
-                previousCity.length() - 1, 1)
+        return currentCity.regionMatches(true, 0, previousCity, previousCity.length() - 1, 1)
                 && !previousCity.equalsIgnoreCase(currentCity);
     }
 
     private static void systemAnswerCity(String inputCity) {
         for (String city : CITIES) {
             if (isSuitableCity(inputCity, city)) {
-                System.out.println("And my answer is... "
-                        + System.lineSeparator() + city
-                        + ". Thank you for playing!");
+                System.out.println("And my answer is... " + System.lineSeparator() + city + ". Thank you for playing!");
                 return;
             }
         }
-        System.out.println("Sorry, I don't have any suitable city in my " +
-                "memory for the answer. You a winner!");
+        System.out.println("Sorry, I don't have any suitable city in my memory for the answer. You a winner!");
     }
 }
